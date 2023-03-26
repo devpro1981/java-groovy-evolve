58ddbdfc13d8429e73d242503208740a07e3f514
msalima <salim.ali@lexmark.com>
2023-03-26 21:40:46 +0530

    More plumbing

--- JvmProvingGrounds/src/main/groovy/Main.groovy
+++ JvmProvingGrounds/src/main/groovy/Main.groovy
@@ -1,3 +1,11 @@
+import com.startup.Creature
+import com.startup.Point
+
 static void main(String[] args) {
   println "Hello world!"
+  Point aPoint = new Point(5.0, 6.0)
+  println(aPoint.toString())
+
+  Creature dangerous = new Creature(45.0, 67.0)
+  println(dangerous.toString())
 }
---
+++ JvmProvingGrounds/src/main/java/com/startup/Creature.java
@@ -0,0 +1,9 @@
+package com.startup;
+
+import lombok.Data;
+
+@Data
+public class Creature {
+    private final double height;
+    private final double weight;
+}
